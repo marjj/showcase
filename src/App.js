c44739dfa731b417be4a4eb395322e96633f4ff7
Marjo Sobrecaray <marjo.sobrecaray@compareclub.com.au>
2025-09-18 23:42:10 +0800

    wip

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-gray-950 w-full max-h-screen h-screen text-white text-sm">
+      <div className="w-full lg:max-w-2xl mx-auto h-full">
+        
+        <div className="grid grid-cols-2">
+          <div className="bg-red-200">1</div>
+          <div className="bg-pink-200">2</div>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+export default App;
