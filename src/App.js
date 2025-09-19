3e1c4c81ba3009e3f4f5b3b630d9eae349d51a5d
Marjo Sobrecaray <marjo.sobrecaray@compareclub.com.au>
2025-09-20 00:34:23 +0800

    wip

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import './App.css';
+import Developer from "./Developer";
 
 function App() {
   return (
-    <div className="bg-gray-950 w-full max-h-screen h-screen text-white text-sm">
+    <div className="bg-[#0e1427] w-full max-h-screen h-screen text-white text-sm">
       <div className="w-full lg:max-w-2xl mx-auto h-full">
         
-        <div className="grid grid-cols-2">
-          <div className="bg-red-200">1</div>
-          <div className="bg-pink-200">2</div>
+        <div className="grid grid-cols-[30%_70%] p-4">
+          <div className="p-4">
+            <Developer />
+          </div>
+          <div className="p-4 text-center">
+            <h2 className="text-4xl font-bold">Hi, I'm a Developer</h2>
+          </div>
         </div>
 
       </div>
---
+++ src/Developer.js
@@ -0,0 +1,25 @@
+import "./developer.css";
+
+function Developer() {
+  return (
+    <div className="p-4 w-full">
+      {/* <div className="laptop"></div> */}
+      <div className="container max-w-max mx-auto relative">
+        <div className="hair"/>
+        <div className="face">
+          <div className="eye"/>
+          <div className="eye-2"/>
+          <div className="blush"/>
+        </div>
+        <div className="bangs bangs-left"/>
+        <div className="bangs bangs-right"/>
+
+        <div className="body"/>
+        <div className="hands"/>
+        <div className="laptop"/>
+
+      </div>
+    </div>
+  );
+}
+export default Developer;
