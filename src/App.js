81355cf243cf58af4f80f61708a70b52818c409d
Marjo Sobrecaray <marjo.sobrecaray@compareclub.com.au>
2025-09-21 00:59:42 +0800

    wip

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 import Developer from "./Developer";
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const [experienceYears, setExperienceYears] = useState(0);
+  const [age, setAge] = useState(0);
+
+
+  useEffect(() => {
+    const year = new Date().getFullYear();
+    const age = year - 1994;
+    setAge(age);
+    setExperienceYears(age - 22);
+    // Your effect logic here
+  }, []);
+
   return (
-    <div className="bg-[#0e1427] w-full max-h-screen h-screen text-white text-sm">
-      <div className="w-full lg:max-w-2xl mx-auto h-full">
-        
-        <div className="grid grid-cols-[30%_70%] p-4">
-          <div className="p-4">
-            <Developer />
+    <div className="bg-[#02081b]">
+      <div className="lg:max-w-2xl mx-auto">
+        <div className="w-full h-screen mx-auto h-full flex flex-col items-center justify-center text-white">
+          <Developer />
+
+          <div className="bg-pink-500 w-20 h-1 rounded-full mt-10"/>
+            <div className="bg-cyan-600 p-4 mt-10 rounded-3xl">
+            <p className="pb-2 font-bold text-3xl">Hi there!</p>
+            <p className="text-sm">A software engineer.</p>
+            <p className="text-sm">I do code.</p>
+            <p className="text-sm">Experience? {experienceYears} years</p>
           </div>
-          <div className="p-4 text-center">
-            <h2 className="text-4xl font-bold">Hi, I'm a Developer</h2>
+
+          <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 text-center text-xs mt-10 text-gray-400">
+            <span>Linkedin</span>
+            <span>Github</span>
+            <span>Mail</span>
+            <span>Codepen</span>
           </div>
         </div>
 
--- src/Developer.js
+++ src/Developer.js
@@ -2,23 +2,19 @@ import "./developer.css";
 
 function Developer() {
   return (
-    <div className="p-4 w-full">
-
-      <div className="container max-w-max mx-auto relative">
-        <div className="hair"/>
-        <div className="face">
-          <div className="eye"/>
-          <div className="eye-2"/>
-          <div className="blush"/>
-        </div>
-        <div className="bangs bangs-left"/>
-        <div className="bangs bangs-right"/>
-
-        <div className="body"/>
-        <div className="hands"/>
-        <div className="laptop"/>
-
+    <div className="container max-w-max mx-auto relative">
+      <div className="hair"/>
+      <div className="face">
+        <div className="eye"/>
+        <div className="eye-2"/>
+        <div className="blush"/>
       </div>
+      <div className="bangs bangs-left"/>
+      <div className="bangs bangs-right"/>
+
+      <div className="body"/>
+      <div className="hands"/>
+      <div className="laptop"/>
     </div>
   );
 }
